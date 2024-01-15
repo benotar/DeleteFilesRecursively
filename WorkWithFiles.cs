553488af975a143e694e0e06b8cc78409f5bfd99
Ivan <zhmur.ivan@gmail.com>
2024-01-15 16:00:25 +0200

    Create method DeleteFile and implement some logic

--- WorkWithFiles.cs
+++ WorkWithFiles.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace HW_15_01_2024;
 
-namespace HW_15_01_2024
+public class WorkWithFiles
 {
-    internal class WorkWithFiles
+    private string _dirPath;
+    public WorkWithFiles(string dirPath)
+        => _dirPath = dirPath;
+
+    void DeleteFile()
     {
+        if (!Directory.Exists(_dirPath))
+        {
+            throw new ArgumentException("Folder not found!");
+        }
+
+        string[] files = Directory.GetFiles(_dirPath);
+
+
     }
 }
